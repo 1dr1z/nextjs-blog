a7845c5d89608ff3d7c2dd4a20b6e61ce880f1dc
Idriz Mehmedovic <idriz.96@hotmail.com>
2022-05-31 20:42:18 +0200

    hero, navbar,posts

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react';
+import FeaturedPosts from '../components/home-page/featured-posts';
+import Hero from '../components/home-page/Hero';
+
+const HomePage = () => {
+  return (
+    <Fragment>
+      <Hero />
+      <FeaturedPosts />
+    </Fragment>
+  );
+};
+
+export default HomePage;
